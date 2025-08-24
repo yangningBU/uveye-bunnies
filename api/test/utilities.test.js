7fb49f9f6616be46414bb7f4e8ff048d155153b7
Yonatan Laurence <yonatanlaurence@gmail.com>
2025-08-24 17:30:00 +0300

    Fix broken tests

--- api/test/utilities.test.js
+++ api/test/utilities.test.js
@@ -21,52 +21,63 @@ function assertEntitiesHaveExpectedFields(result, expectedEntities) {
         entity[key],
         matchingResult[key],
         `Supposed to be ${entity[key]} but got ${matchingResult[key]} ` +
-        `for ${key}`,
+        `for ${key}. Expected: ${JSON.stringify(entity)}, ` +
+        `Result: ${JSON.stringify(matchingResult)}`,
       );
     });
   });
 }
 
+const finalTimestampInSnapshotOneEventBundle = new Date("2023-01-01T00:08:00Z");
 const simpleTimeline = [
   {
     eventType: EVENTS.bunny.created,
     id: 100,
     name: "Arnold",
+    timestamp: new Date("2023-01-01T00:00:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 100,
+    timestamp: new Date("2023-01-01T00:01:00Z"),
   },
   {
     eventType: EVENTS.bunny.created,
     id: 101,
     name: "Fluffy",
+    timestamp: new Date("2023-01-01T00:02:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 101,
+    timestamp: new Date("2023-01-01T00:03:00Z"),
   },
   {
     eventType: EVENTS.bunny.lettuceEaten,
     bunnyId: 100,
+    timestamp: new Date("2023-01-01T00:04:00Z"),
   },
   {
     eventType: EVENTS.bunny.created,
     id: 102,
     name: "Ralphy",
+    timestamp: new Date("2023-01-01T00:05:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 102,
+    timestamp: new Date("2023-01-01T00:06:00Z"),
   },
   {
     eventType: EVENTS.bunny.lettuceEaten,
     bunnyId: 101,
+    timestamp: new Date("2023-01-01T00:07:00Z"),
   },
   {
     eventType: EVENTS.bunny.playDateHad,
     bunnyId: 101,
     otherBunnyId: 102,
+    timestamp: finalTimestampInSnapshotOneEventBundle,
   },
 ];
 
@@ -107,23 +118,28 @@ const timeline2 = [
     eventType: EVENTS.bunny.created,
     id: 200,
     name: "Howard",
+    timestamp: new Date("2023-01-02T00:00:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 100,
+    timestamp: new Date("2023-01-02T00:01:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 101,
+    timestamp: new Date("2023-01-02T00:02:00Z"),
   },
   {
     eventType: EVENTS.bunny.carrotsEaten,
     bunnyId: 200,
+    timestamp: new Date("2023-01-02T00:03:00Z"),
   },
   {
     eventType: EVENTS.bunny.playDateHad,
     bunnyId: 100,
     otherBunnyId: 200,
+    timestamp: new Date("2023-01-02T00:04:00Z"),
   },
 ];
 
@@ -205,7 +221,10 @@ describe("utilities", () => {
       const dbMatch = snapshot1.entities.find((b) => b.id === bunnyId);
       if (dbMatch) {
         return {
-          bunny: { ...dbMatch },
+          bunny: {
+            ...dbMatch,
+            lastEventAppliedTimestamp: finalTimestampInSnapshotOneEventBundle,
+          },
           alreadyInList: false,
         };
       }
